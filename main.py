6fd26d2aa8137baf85b16a2c53fc2a0d59676cf4
dhanoosu <dhanoo.surasarang@gmail.com>
2023-03-11 14:46:41 +0000

    [Feature] Adding splash ascii art during initialisation

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+SPLASH_ASCII = """
+█████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ 
+                                                                              
+                                                                              
+     █████  ███    ██ ████████     ███████ ██   ██ ███████ ██      ██         
+    ██   ██ ████   ██    ██        ██      ██   ██ ██      ██      ██         
+    ███████ ██ ██  ██    ██        ███████ ███████ █████   ██      ██         
+    ██   ██ ██  ██ ██    ██             ██ ██   ██ ██      ██      ██         
+    ██   ██ ██   ████    ██        ███████ ██   ██ ███████ ███████ ███████    
+                                                                              
+                                                                              
+    Version: x.x.x
+                                                                              
+█████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ 
+                                                                              
+"""
+
 from TkTerm.tkterm import *
 from AntInterpreter import AntInterpreter
 
@@ -5,7 +22,7 @@ root = tk.Tk()
 root.title("Ant Terminal")
 root.geometry("700x400")
 
-terminal = Terminal(root)
+terminal = Terminal(root, bd=0, text=SPLASH_ASCII)
 terminal.pack(fill=BOTH, expand=True)
 
 interpreter = AntInterpreter()
